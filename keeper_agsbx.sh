e2021bf49ef1689b4cf45311ecb81b42bf482afd
xd88dx <xd88dx@foxmail.com>
2025-09-27 16:54:23 +0800

    update keeper

--- keeper_agsbx.sh
+++ keeper_agsbx.sh
@@ -3,15 +3,31 @@
 check_type="${1:-all}"
 log_file="$(pwd)/log_agsbx.log"
 
-# 校验参数
-if [[ ! "$check_type" =~ ^(sb|all)$ ]]; then
-  log "无效参数: $check_type，只支持 sb、all"
-  exit 1
-fi
-
 log() {
   echo "$(date +'%Y-%m-%d %H:%M:%S') $*" | tee -a "$log_file"
 }
+
+run_res() {
+  echo >"$log_file"
+  log "$1"
+
+  output=$(bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosbx/main/argosbx.sh) res 2>&1)
+  echo "$output" >>"$log_file"
+
+  if echo "$output" | grep -q "未安装"; then
+    log "检测到未安装状态，执行部署脚本..."
+    "$(pwd)/deploy_agsbx.sh" >>"$log_file" 2>&1
+  fi
+}
+
+# 校验参数
+if [[ ! "$check_type" =~ ^(sb|all|keep)$ ]]; then
+  log "无效参数: $check_type，只支持 sb、all、keep"
+  exit 1
+fi
+
+# 每日重启
+[[ "$check_type" == "keep" ]] && run_res "每日重启" && exit 0
 
 # 进程检测
 sing_exists=1
@@ -29,14 +45,5 @@ all)
   ;;
 esac
 
-# 执行保活
-echo >"$log_file"
-log "进程检测失败, 开始重启..."
-
-curl_output=$(bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosbx/main/argosbx.sh) res 2>&1)
-echo "$curl_output" >>"$log_file"
-
-if echo "$curl_output" | grep -q "未安装"; then
-  log "检测到未安装状态，执行部署脚本..."
-  "$(pwd)/deploy_agsbx.sh" >>"$log_file" 2>&1
-fi
+# 持久保活
+run_res "进程检测失败, 开始重启..."
